a9095e88352821440e2cb61687be48fcdebd3e34
Steven Gutierrez <stevogbmv@gmail.com>
2023-09-29 16:28:53 -0700

    commit tree 67b9e81b714d9e4aeb4d444298fa56f411b9e1c5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
+export function createStore(reducer) {
   // write your createStore code here
+  let state;
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  return { getState, dispatch };
 }
 
 export function candyReducer(state = [], action) {
@@ -12,7 +23,6 @@ export function candyReducer(state = [], action) {
       return state;
   }
 }
-
 function render() {
   let container = document.getElementById("container");
   if (store.getState()) {
@@ -21,6 +31,8 @@ function render() {
     throw new Error("the store's state has not been defined yet");
   }
 }
+let store = createStore(candyReducer);
+store.dispatch({ type: "@@INIT" });
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
